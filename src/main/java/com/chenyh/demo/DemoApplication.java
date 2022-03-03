75ec0598cce6249d64a134997aaa49b981cfce97
chenyh <chenyh.me@gmail.com>
2022-03-04 07:33:52 +0800

    feat: send log to kafka

--- src/main/java/com/chenyh/demo/DemoApplication.java
+++ src/main/java/com/chenyh/demo/DemoApplication.java
@@ -1,19 +1,25 @@
 package com.chenyh.demo;
 
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @SpringBootApplication
 @RestController
 public class DemoApplication {
 
- 	@RequestMapping("/")
-  	public String home() {
-    		return "Hello Docker World";
-  	}
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	@RequestMapping("/")
+	public String home() {
+		logger.error("-------------");
+		return "Hello Docker World";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
